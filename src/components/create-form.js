dde1d1594b77fbcd6a9971cbbc89fe2699fb1ce7
Ryan Emmans <ryanemmans@gmail.com>
2021-08-22 00:46:54 -0700

    Added image, styled header.

--- src/components/create-form.js
+++ src/components/create-form.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Input } from 'semantic-ui-react'
-import { Grid, Segment, Header, Form, Label, Button, Icon } from 'semantic-ui-react'
+import { Grid, Segment, Header, Form, Label, Button, Icon, Image } from 'semantic-ui-react'
 
 const sizes = [
     'mini',
@@ -16,10 +16,15 @@ const sizes = [
 const style = {
     segment: {
         marginBottom: '50px',
+        marginLeft: '35px',
     },
     label: {
-        border:'none',
+        border: 'none',
         verticalAlign: 'middle',
+    },
+    cookie: {
+        filter: 'invert(39%) sepia(89%) saturate(552%) hue-rotate(164deg) brightness(98%) contrast(90%)',
+        marginTop: '15px',
     }
 }
 
@@ -35,44 +40,51 @@ export default function CreateForm({ onCreate }) {
         event.target.reset();
     }
     return (
-        <Grid textAlign='center' container columns={1}>
-            <Segment raised compact color='blue' textAlign='center' style={style.segment}>
-                <Form onSubmit={submitHandler}>
-                    <Header as='h2'>Create Cookie Stand</Header>
-                    <Form.Group>
-                        <Form.Field inline>
-                            <Label basic htmlFor="location" size={'big'} style={style.label}><Icon name='globe' />Location</Label>
+        <Grid fluid textAlign='center'>
+            <Grid.Row>
+                <Grid.Column width={7}>
+                <Segment raised compact color='blue' textAlign='center' style={style.segment}>
+                    <Form onSubmit={submitHandler}>
+                        <Header as='h2'>Create Cookie Stand</Header>
+                        <Form.Group>
+                            <Form.Field inline>
+                                <Label basic htmlFor="location" size={'large'} style={style.label}><Icon name='globe' />Location</Label>
 
-                            <Input type="text" name="location" placeholder='Enter Store...' />
-                        </Form.Field>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Field inline>
-                            <Label basic htmlFor="min-customers" size={'big'} style={style.label}><Icon name='user' />Min Customers per Hour</Label>
+                                <Input type="text" name="location" placeholder='Enter Store...' />
+                            </Form.Field>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Field inline>
+                                <Label basic htmlFor="min-customers" size={'large'} style={style.label}><Icon name='user' />Min Customers per Hour</Label>
 
-                            <Input type="number" name="min-customers" placeholder='Quantity' />
-                        </Form.Field>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Field inline>
-                            <Label basic htmlFor="max-customers" size={'big'} style={style.label}><Icon name='users' />Max Customers per Hour</Label>
+                                <Input type="number" name="min-customers" placeholder='Quantity' />
+                            </Form.Field>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Field inline>
+                                <Label basic htmlFor="max-customers" size={'large'} style={style.label}><Icon name='users' />Max Customers per Hour</Label>
 
-                            <Input type="number" name="max-customers" placeholder='Quantity' />
-                        </Form.Field>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Field inline>
-                            <Label basic htmlFor="avg-cookies" size={'big'} style={style.label}><Icon name='dollar' />Average Cookies per Sale</Label>
+                                <Input type="number" name="max-customers" placeholder='Quantity' />
+                            </Form.Field>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Field inline>
+                                <Label basic htmlFor="avg-cookies" size={'large'} style={style.label}><Icon name='dollar' />Average Cookies per Sale</Label>
 
-                            <Input type="number" name="avg-cookies" step=".1" placeholder='Quantity' />
-                        </Form.Field>
-                    </Form.Group>
-                    <Button fluid size={'big'} animated='fade' type="submit" color='blue'>
-                        <Button.Content visible>Create</Button.Content>
-                        <Button.Content hidden><Icon name='plus circle' /></Button.Content>
-                    </Button>
-                </Form >
-            </Segment>
-        </Grid>
+                                <Input type="number" name="avg-cookies" step=".1" placeholder='Quantity' />
+                            </Form.Field>
+                        </Form.Group>
+                        <Button fluid size={'big'} animated='fade' type="submit" color='blue'>
+                            <Button.Content visible>Create</Button.Content>
+                            <Button.Content hidden><Icon name='plus circle' /></Button.Content>
+                        </Button>
+                    </Form >
+                </Segment>
+            </Grid.Column>
+            <Grid.Column width={7} textAlign='center'>
+            <Image src='https://www.nicepng.com/png/full/5-50567_cookie-vector-png-clipart-transparent-download-black-and.png' size='medium' verticalAlign='middle' style={style.cookie} />
+            </Grid.Column>
+            </Grid.Row>
+        </Grid >
     )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,13 @@ import { Header } from 'semantic-ui-react'
 
 const style = {
     h1: {
-      margin: '50px',
+      margin: '40px',
       fontSize: '3em',
       textTransform: 'uppercase',
+      backgroundColor: '#f9fafb',
+      borderRadius: '5px',
+      border: '1px solid #dfdfdf',
+      borderTop: '2px solid #2085d0',
     },
   }
 
